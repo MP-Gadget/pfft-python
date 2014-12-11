479d814972a40df211e8ee979911661a8c14d70e
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2014-12-10 21:01:17 -0500

    more elobrated example.

--- example.py
+++ example.py
@@ -1,8 +1,19 @@
 from mpi4py import MPI
-
+import numpy
 import pfft
 
+if MPI.COMM_WORLD.rank == 0:
+    print \
+        """
+This example performs a in-place transform, with a naive slab decomposition.
+
+In place transform is achieved by providing a single buffer object to pfft.Plan.
+Consequently, calls to plan.execute we also provide only a single buffer object.
+"""
+
+
 procmesh = pfft.ProcMesh([4])
+
 partition = pfft.Partition(
         pfft.Type.PFFT_C2C, 
         [8, 8], 
@@ -18,8 +29,26 @@ iplan = pfft.Plan(partition, pfft.Direction.PFFT_BACKWARD, buffer,
         )
 
 input = buffer.view_input()
-input[...] = 1.0
+
+# now lets fill the input array in a funny way
+# a[i, j] = i * 10 + j
+# we will do a tranform roundtrip and print this out
+indices = numpy.array(numpy.indices(input.shape))
+indices += partition.local_i_start[:, None, None]
+i, j = indices
+input[...] = i * 10 + j
+
 plan.execute(buffer)
+
 output = buffer.view_output()
+
+# denormalize the forward transform 
+output /= numpy.product(partition.n)
+
 iplan.execute(buffer)
-print input
+
+ginput = MPI.COMM_WORLD.gather(input)
+if MPI.COMM_WORLD.rank == 0:
+    print 'You shall see an array of form i * 10 + j'
+    for item in ginput:
+        print item
