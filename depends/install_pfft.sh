72157102214dc9477796f5e6d1d45d67b5594145
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-10-09 11:20:12 -0700

    Trying to fix the bad substitution again.

--- depends/install_pfft.sh
+++ depends/install_pfft.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 PREFIX="$1"
 shift
 OPTIMIZE="$*"
+OPTIMIZE1=`echo "$*" | sed -s 's;enable-sse2;enable-sse;'`
+echo ${OPTIMIZE}
+echo ${OPTIMIZE1}
+
 PFFT_VERSION=1.0.8-alpha-fftw3
 TMP="tmp-pfft-$PFFT_VERSION"
 LOGFILE="build.log"
@@ -24,22 +28,24 @@ ROOT=`dirname $0`/../
 wget https://github.com/rainwoodman/pfft/releases/download/$PFFT_VERSION/pfft-$PFFT_VERSION.tar.gz \
     -O $ROOT/depends/pfft-$PFFT_VERSION.tar.gz 
 
-gzip -dc $ROOT/depends/pfft-$PFFT_VERSION.tar.gz | tar xvf - -C $TMP
+gzip -dc $ROOT/depends/pfft-$PFFT_VERSION.tar.gz | tar xf - -C $TMP
 cd $TMP
 
-cd pfft-$PFFT_VERSION
-
-./configure --prefix=$PREFIX --disable-shared --enable-static  \
+(
+mkdir double
+cd double
+../pfft-${PFFT_VERSION}/configure --prefix=$PREFIX --disable-shared --enable-static  \
 --disable-fortran --disable-doc --enable-mpi ${OPTIMIZE}
-2>&1 | tee $LOGFILE
+make -j 4 
+make install 
+) 2>&1 | tee ${LOGFILE}.double |tail
 
-make -j 4 2>&1 | tee $LOGFILE
-make install 2>&1 | tee $LOGFILE
-
-./configure --prefix=$PREFIX --enable-single --disable-shared --enable-static  \
---disable-fortran --disable-doc --enable-mpi $2 ${OPTIMIZE/--enable-sse2/--enable-sse}
-2>&1 | tee $LOGFILE
-
-make -j 4 2>&1 | tee $LOGFILE
-make install 2>&1 | tee $LOGFILE
+(
+mkdir single
+cd single
+../pfft-${PFFT_VERSION}/configure --prefix=$PREFIX --enable-single --disable-shared --enable-static  \
+--disable-fortran --disable-doc --enable-mpi $2 ${OPTIMIZE1}
+make -j 4 
+make install
+) 2>&1 | tee ${LOGFILE}.single | tail
 
