d84dd0324da1174b130be1c24c5eefb60c9dfe89
Yu Feng <rainwoodman@gmail.com>
2015-11-18 21:39:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -59,12 +59,12 @@ extensions = [
 
 build_pfft()
 setup(
-    name="pfft-python", version="0.1pre",
+    name="pfft-python", version="0.1",
     author="Yu Feng",
     author_email="rainwoodman@gmail.com",
     description="python binding of PFFT, a massively parallel FFT library",
     url="http://github.com/rainwoodman/pfft-python",
-    download_url="https://github.com/rainwoodman/pfft-python/releases/download/v0.1pre/pfft-python-0.1pre.tar.gz",
+    download_url="https://github.com/rainwoodman/pfft-python/archive/v0.1.zip",
     #package_dir = {'pfft': 'pfft'},
     zip_safe=False,
     install_requires=['cython', 'numpy'],
