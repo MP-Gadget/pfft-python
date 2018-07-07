05af81877d41dc185f54937bb00dbb064d122dd8
Yu Feng <rainwoodman@gmail.com>
2018-07-06 18:33:50 -0700

    fix a few problems regarding the dangerous API.
    0.1.21 shall be better (see comment).
    
    Need to tag a release after travis is green.

--- examples/elasticwave.py
+++ examples/elasticwave.py
@@ -5,25 +5,26 @@ import numpy
 
 def main(comm):
     Nmesh = [8, 8]
+
     if len(Nmesh) == 3:
         procmesh = pfft.ProcMesh(pfft.split_size_2d(comm.size), comm=comm)
     else:
         procmesh = pfft.ProcMesh((comm.size,), comm=comm)
 
     partition = pfft.Partition(
-        pfft.Type.PFFT_R2C,
+        pfft.Type.R2C,
         Nmesh,
         procmesh,
-        pfft.Flags.PFFT_TRANSPOSED_OUT | pfft.Flags.PFFT_DESTROY_INPUT
+        pfft.Flags.PADDED_R2C | pfft.Flags.PFFT_TRANSPOSED_OUT | pfft.Flags.DESTROY_INPUT
         )
 
     # generate the coordinate support.
 
     k = [None] * partition.ndim
-    x = [None] * parititon.ndim
+    x = [None] * partition.ndim
     for d in range(partition.ndim):
         k[d] = numpy.arange(partition.no[d])[partition.local_o_slice[d]]
-        k[d][k[d] > partition.no[d] // 2] -= partition.no[d]
+        k[d][k[d] >= partition.n[d] // 2] -= partition.n[d]
         # set to the right numpy broadcast shape
         k[d] = k[d].reshape([-1 if i == d else 1 for i in range(partition.ndim)])
 
@@ -38,8 +39,13 @@ def main(comm):
     buffer2 = pfft.LocalBuffer(partition)
     phi_spec = buffer2.view_output()
 
-    # forward plan
-    disp_to_spec = pfft.Plan(partition, pfft.Direction.PFFT_FORWARD, buffer1, buffer2)
+    # forward plan 
+    disp_to_spec_inplace = pfft.Plan(partition, pfft.Direction.PFFT_FORWARD,
+                    buffer2, buffer2,
+                    # the two lines below not needed after version 0.1.21
+                    # type=pfft.Type.R2C,
+                    # flags=pfft.Flags.TRANSPOSED_OUT | pfft.Flags.DESTROY_INPUT | pfft.Flags.PADDED_R2C
+                    )
 
     buffer3 = pfft.LocalBuffer(partition)
     grad_spec = buffer3.view_output()
@@ -48,7 +54,12 @@ def main(comm):
     grad_disp = buffer4.view_input()
 
     # backward plan
-    spec_to_disp = pfft.Plan(partition, pfft.Direction.PFFT_FORWARD, buffer3, buffer4)
+    spec_to_disp = pfft.Plan(partition, pfft.Direction.PFFT_BACKWARD,
+                    buffer3, buffer4,
+                    # the two lines below not needed after version 0.1.21
+                    # type=pfft.Type.C2R,
+                    # flags=pfft.Flags.TRANSPOSED_IN | pfft.Flags.DESTROY_INPUT | pfft.Flags.PADDED_C2R
+                    )
 
     # to do : fill in initial value
     dx = x[0] - Nmesh[0] * 0.5 + 0.5
@@ -57,17 +68,18 @@ def main(comm):
 
     cprint('phi =', gather(partition, phi_disp).round(2), comm=comm)
 
-    disp_to_spec.execute(phi_disp.base, phi_spec.base)
+    # copy in to the buffer for inplace transform
+    # this preserves value of phi_disp
+    phi_spec.base.view_input()[...] = phi_disp
+    disp_to_spec_inplace.execute(phi_spec.base, phi_spec.base)
 
     all_grad_disp = numpy.zeros([partition.ndim] + list(phi_disp.shape), dtype=grad_disp.dtype)
 
-    cprint('phi_k =', gather(partition, phi_spec, mode='output').round(2), comm=comm)
+#    cprint('phi_k =', gather(partition, phi_spec, mode='output').round(2), comm=comm)
 
     for d in range(partition.ndim):
         grad_spec[...] = phi_spec[...] * (k[d] * 1j)
-        cprint('grad_k =', gather(partition, grad_spec, mode='output').round(2), comm=comm)
-
-        disp_to_spec.execute(grad_spec.base, grad_disp.base)
+        spec_to_disp.execute(grad_spec.base, grad_disp.base)
         # copy the gradient along d th direction
         all_grad_disp[d] = grad_disp
 
